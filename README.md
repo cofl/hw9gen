67ae94946ac1068c10d388594cb4bd8bd4563089
Christian LaCourt <cllacour@c28-0112-02.ad.mtu.edu>
2015-11-17 15:23:49 -0500

    Fixed empty task list bug, added version CLI option, internal rewrites.

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# HW9 Gen by Christian LaCourt
+# HW9 Gen
+by Christian LaCourt
 
 Generates sample input for MTU CS1131 Fall 2015 HW9.
 
@@ -16,11 +17,13 @@ If running from a jar:
 
 ### Options:
 
-- `-c, --cores <arg>`: The number of CPU cores to be used in the simulator. Defaults to 4.
 - `-h, --help`: Shows the help text.
+- `-v, --version`: Shows the current version of the program.
+- `-c, --cores <arg>`: The number of CPU cores to be used in the simulator. Defaults to 4.
+- `-p, --num-processes <arg>`: The number of processes to be generated. Defaults to 1.
+- `-m, --max-commands <arg>`: The maximum number of commands to generate per process. Defaults to 150.
 - `-n, --allow-negatives`: Turn on generating negative arguments for CPU and Disk operations. Defaults to off.
 - `-o, --output-file <arg>`: Set the file to output to. If this is not provided, the output is printed to the standard output.
-- `-p, --num-processes <arg>`: The number of processes to be generated. Defaults to 1.
 - `-s, --strict`: Enable strict mode (don't generate repeats as the first task). Defaults to off.
 - `-d, --disable-output`: Disable printing to standard out (only applies if an ouput file has been set). Defaults to off.
 - `-W<name>=<weight>`: Set custom weighting for each generator
@@ -28,3 +31,9 @@ If running from a jar:
   - repeat: defaults to 12
   - usedisk: defaults to 9
   - usecpu: defaults to 14
+
+### Examples:
+- `java -jar gen.jar -s | demo.hw9`
+  Pipes some input to the demo program (sorry interwebs, that program's not for you)
+- `java -jar gen.jar -n -p 64 -c 16 -m 250 -Wusedisk=30 -Wwait=0 -o sample.input | java -cp ./bin simulator.Main`
+  Creates 64 processes no longer than 250 commands to run on a 16-core CPU with the extra credit check enable, with an emphasis on disk operations and waits disabled. Outputs the input to `sample.input` and standard output, which is then piped to your code.
--- src/Gen.java
+++ src/Gen.java
@@ -4,6 +4,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.HashMap;
+import java.util.Properties;
 import java.util.Random;
 import java.util.TreeMap;
 import java.util.function.Supplier;
@@ -21,37 +22,47 @@ import org.apache.commons.cli.ParseException;
  * @author Christian LaCourt
  */
 public class Gen {
+
+    /**
+     * A command generator.
+     */
     private static class Generator implements Supplier<String> {
         private String name;
         private Supplier<String> generator;
-        Generator(String name, Supplier<String> generator){
+
+        Generator(String name, Supplier<String> generator) {
             this.name = name;
             this.generator = generator;
         }
-        String getName(){
+
+        String getName() {
             return name;
         }
-        @Override public String get(){
+
+        @Override
+        public String get() {
             return generator.get();
         }
     }
+
     private static int level = 0; // indentation, multiples of 2.
     private static boolean genNegatives = false; // generate negative values?
-    private static boolean strictMode = false;
-    private static boolean disableOutput = false;
+    private static boolean strictMode = false; // don't generate a repeat at the start of a process
+    private static boolean disableOutput = false; // disable standard output when outputting to a file
     private static boolean justGeneratedRepeat = false; // just generated a repeat tasks's start?
     private static PrintStream out = System.out;
 
     private static final Random random = new Random(); // rng
-    private static int MAX_COMMANDS = 150; // max number of commands a process can have.
+    private static int MAX_COMMANDS = 150; // max number of commands a process can have. (> 5)
+
+    private static final TreeMap<Integer, Generator> generators = new TreeMap<>();
+    private static final HashMap<String, Integer> weights = new HashMap<>();
+    private static int MAX_WEIGHT;
 
     public static void main(String... args) {
         int cores = 4;
         int nproc = 1;
 
-        int max = 0; // maximum key in generators
-        TreeMap<Integer, Generator> generators = new TreeMap<>();
-        HashMap<String, Integer> weights = new HashMap<>();
         weights.put("wait", 2);
         weights.put("repeat", 12);
         weights.put("usedisk", 9);
@@ -67,19 +78,24 @@ public class Gen {
             options.addOption("s", "strict", false, "Enable strict mode (don't generate repeats as the first task).");
             options.addOption("d", "disable-output", false, "Disable printing to standard out (only applies if an ouput file has been set).");
             options.addOption("m", "max-commands", true, "Set the maximum number of commands to generate (Must not be less than 5).");
+            options.addOption("v", "version", false, "Shows the current version of the program.");
             options.addOption(Option.builder("W").hasArgs().valueSeparator('=').build());
             CommandLine cmd = new DefaultParser().parse(options, args);
             if(cmd.hasOption("h")) {
                 HelpFormatter f = new HelpFormatter();
+                System.out.println("hw9gen v0.3.0 Cordial Cobalt");
                 String usage = "java -cp ../lib/commons-cli-1.3.1/commons-cli-1.3.1.jar;./ Gen OR java -jar Gen.jar";
                 f.printHelp(usage, "Generates random input for MTU CS1131 Fall 2015 HW9.", options, "", true);
                 System.exit(0);
             }
-            if(cmd.hasOption("W")){
-                String[] opts = cmd.getOptionValues("W");
-                for(int i = 0; i < opts.length; i += 2){
-                    weights.put(opts[i].toLowerCase(), Integer.parseInt(opts[i + 1]));
-                }
+            if(cmd.hasOption("v")) {
+                System.out.println("hw9gen v0.3.0 Cordial Cobalt");
+                System.exit(0);
+            }
+            if(cmd.hasOption("W")) {
+                Properties opts = cmd.getOptionProperties("W");
+                for(final String name: opts.stringPropertyNames())
+                    weights.put(name, Math.max(0, Integer.parseInt(opts.getProperty(name))));
             }
             strictMode = cmd.hasOption("s");
             genNegatives = cmd.hasOption("n");
@@ -91,58 +107,47 @@ public class Gen {
             if(cmd.hasOption("o"))
                 out = new PrintStream(new FileOutputStream(new File(cmd.getOptionValue("o"))));
             if(cmd.hasOption("m"))
-                MAX_COMMANDS = Integer.parseInt(cmd.getOptionValue("m"));
+                MAX_COMMANDS = Math.max(5, Integer.parseInt(cmd.getOptionValue("m")));
         } catch(ParseException | NumberFormatException | IOException e) {
             System.err.println("Fatal error while parsing command-line options");
             System.exit(1);
         }
 
         // == WAIT ==
-        generators.put(0, new Generator("wait", () -> String.format("%" + level + "swait %d", "", random.nextInt(100) + 1))); 
-        max += weights.get("wait");
+        addGenerator(new Generator("wait", () -> String.format("%" + level + "swait %d", "", random.nextInt(100) + 1)));
 
         // == REPEAT ==
-        generators.put(max, new Generator("repeat", () -> {
-            if(random.nextBoolean() && level > 2 && !justGeneratedRepeat) {
+        addGenerator(new Generator("repeat", () -> {
+            if(random.nextBoolean() && level > 2) {
                 level -= 2;
-                justGeneratedRepeat = false;
                 return String.format("%" + level + "s;", "");
             }
-            if(level > 12 || justGeneratedRepeat) {
+            if(level > 12) {
                 // Don't generate very deep repeats.
-                justGeneratedRepeat = false;
                 return String.format("%" + level + "swait %d", "", random.nextInt(100) + 1);
             }
-            String text = String.format("%" + level + "srepeat %d", "", random.nextInt(level > 6?5:13) + 2);
+            String text = String.format("%" + level + "srepeat %d", "", random.nextInt(level > 6 ? 5 : 13) + 2);
             level += 2;
             justGeneratedRepeat = true;
             return text;
         }));
-        max += weights.get("repeat");
 
         // == USE DISK ==
-        generators.put(max, new Generator("usedisk", () -> String.format("%" + level + "suse Disk %d", "", (random.nextBoolean() && genNegatives)?-1:random.nextInt(1024))));
-        max += weights.get("usedisk");
+        addGenerator(new Generator("usedisk", () -> String.format("%" + level + "suse Disk %d", "", (random.nextBoolean() && genNegatives) ? -1 : random.nextInt(1024))));
 
         // == USE CPU ==
-        generators.put(max, new Generator("usecpu", () -> String.format("%" + level + "suse CPU %d", "", (random.nextDouble() < 0.1 && genNegatives?-1:1) * random.nextInt(random.nextDouble() < 0.2?100:1500))));
-        max += weights.get("usecpu");
+        addGenerator(new Generator("usecpu", () -> String.format("%" + level + "suse CPU %d", "", (random.nextDouble() < 0.1 && genNegatives ? -1 : 1) * random.nextInt(random.nextDouble() < 0.2 ? 100 : 1500))));
 
         writeln(cores);
         for(int cproc = 1; cproc <= nproc; cproc++) {
             writef("process%d%n", cproc);
             level = 2;
-            int numCommands = getNumCommands();
-            if(strictMode){
-                numCommands--;
-                Generator g = generators.floorEntry(random.nextInt(max)).getValue();
-                while(g.getName().equals("repeat"))
-                    g = generators.floorEntry(random.nextInt(max)).getValue();
-                writeln(g.get());
-            }
-            for(; numCommands > 0; numCommands--) {
-                writeln(generators.floorEntry(random.nextInt(max)).getValue().get());
-            }
+            justGeneratedRepeat = strictMode; // not really, but tricks the generator.
+            for(int numCommands = getNumCommands(); numCommands > 1; numCommands--)
+                generate();
+            // next, force the last command to not be a repeat.
+            justGeneratedRepeat = true;
+            generate();
             while(level > 0) {
                 level -= 2;
                 if(level == 0) {
@@ -159,34 +164,66 @@ public class Gen {
         }
     }
 
+    /**
+     * Adds a generator to the list.
+     */
+    private static void addGenerator(Generator generator) {
+        int weight = weights.getOrDefault(generator.getName(), 1);
+        if(weight == 0)
+            return;
+        generators.put(MAX_WEIGHT += weight, generator);
+    }
+
+    /**
+     * Generates a command.
+     */
+    private static void generate() {
+        Generator g;
+        do
+            g = generators.ceilingEntry(random.nextInt(MAX_WEIGHT)).getValue();
+        while(justGeneratedRepeat && g.getName().equals("repeat"));
+        if(justGeneratedRepeat && !g.getName().equals("repeat"))
+            justGeneratedRepeat = false;
+        writeln(g.get());
+    }
+
+    /**
+     * Gets the number of commands to generate by Gaussian distribution from 4 to MAX_COMMANDS.
+     */
     private static int getNumCommands() {
         double range = 1.8;
         double distrib = random.nextGaussian() + range / 2;
         if(distrib < 0)
             distrib = 0;
-        if(distrib > range)
+        else if(distrib > range)
             distrib = range;
         return (int) Math.round((MAX_COMMANDS - 4) * distrib / range) + 4;
     }
-    
-    private static void writef(String formatString, Object... arguments){
-        if(out != System.out){
+
+    /**
+     * Write to all outputs.
+     */
+    private static void writef(String formatString, Object... arguments) {
+        if(out != System.out) {
             out.printf(formatString, arguments);
-            if(!disableOutput){
+            if(!disableOutput) {
                 System.out.printf(formatString, arguments);
             }
-        } else if(!disableOutput){
+        } else if(!disableOutput) {
             System.out.printf(formatString, arguments);
         }
     }
-    
-    private static void writeln(Object string){
-        if(out != System.out){
+
+    /**
+     * Write to all outputs.
+     */
+    private static void writeln(Object string) {
+        if(out != System.out) {
             out.println(string);
-            if(!disableOutput){
+            if(!disableOutput) {
                 System.out.println(string);
             }
-        } else if(!disableOutput){
+        } else if(!disableOutput) {
             System.out.println(string);
         }
     }
